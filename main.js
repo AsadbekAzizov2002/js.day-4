36d0210b10bd3406c8f7ee14fa8c9e4cc51abcb0
AsadbekAzizov2002 <azizovasadbek489@gmail.com>
2023-08-10 00:13:30 +0500

    end

---
+++ main.js
@@ -0,0 +1,217 @@
+// let a = "scbsbcsakerbjhbjhwk642665wgewrf5454125egr"
+
+// let resultnum = 0;
+// let resultstr = "";
+ 
+// for (let i = 0; i<a.length; i++){
+//     console.log(a[i]);
+// }
+
+
+
+// let a = "scbsbcsakerbjhbjhwk642665wgewrf5454125egr"
+
+// let resultnum = 0;
+// let resultstr = "";
+ 
+// for (let i = 0; i<a.length; i++){
+//     console.log(Number(a[i]));
+// }
+
+
+
+
+// let a = "scbsbcsakerbjhbjhwk642665wgewrf5454125egr"
+
+// let resultnum = 0;
+// let resultstr = "";
+ 
+// for (let i = 0; i<a.length; i++){
+//     let sum = parseInt (a[i]);
+//     if (sum){
+//         resultnum+= sum;
+//     }
+// }
+// console.log(resultnum);
+
+
+
+// let a = "scbsbcsakerbjhbjhwk642665wgewrf5454125egr"
+
+// let resultnum = 0;
+// let resultstr = "";
+ 
+// for (let i = 0; i<a.length; i++){
+//     let sum = parseInt (a[i]);
+//     if (sum){
+//         resultnum+= sum;
+//     }else {
+//         resultstr += a[i];
+//     }
+// }
+// console.log(resultnum);
+// console.log(resultstr);
+
+
+
+// let a = "scbsbcsakerbj00hbjhwk6420665wgew0rf5454125egr"
+
+// let resultnum = 0;
+// let resultstr = "";
+ 
+// for (let i = 0; i<a.length; i++){
+//     let sum = parseInt (a[i]);
+//     if (sum ||sum ==0){
+//         resultnum+= sum;
+//     }else {
+//         resultstr += a[i];
+//     }
+// }
+// console.log(resultnum);
+// console.log(resultstr);
+
+
+
+// for (let i=0; i<10; i++){
+//     console.log("~~~~~~~~~~ ", i , "///////////////////////");
+//     for (let l=0 ; l<3 ; l++)
+//     console.log(l);
+// }
+
+
+
+// let i = 0;
+// while(i < 5){
+
+//     console.log(i);
+//     i++
+// }
+
+
+
+// let i = 0;
+// while(i<5){
+//     let j = 0;
+//     console.log("+++++++++++++",i,"-----------------");
+//     while(j<3){
+//         console.log(j);
+//         j++;
+//     }
+//     i++;
+// }
+
+
+
+
+// let i = 0;
+
+// do{
+//     console.log(i);
+//     i++
+// }while(i<0)
+
+
+
+// tubson 
+
+// let num1 = Number(prompt("son kiriting"));
+// let num2 = false;
+// let num3 = 1;
+// if(num1>0){
+//     for (let i = 2; i<num1; i++) {
+
+
+//         if(num1 % i ==0){
+
+//             num2=true
+//         }
+//     }
+//     if(num2==false){
+//         console.log(num1,"tub son")
+            
+//     }else {
+//                 console.log(num1, "oddiy son");
+//             }
+//         }else{
+//             console.log("manfiy son")
+//     }
+
+
+
+// kiritilgan son ichidagi tub sonlarni topish 
+
+// let num1 = Number(prompt("ihtiyoriy son tanlang"));
+
+// let num3 = 0;
+// if (num1>0){
+//     for (let i = 2; i<i; i++){
+//         let num2 = true
+//         for (let j = 2; j<i; j++) {
+//             if (i%j ==0){
+//                 num2 = false
+//                 break
+//              }
+//         }
+//          if (num2) {
+//             console.log(i,"tub");
+//         } 
+//     }
+    
+//     let count = 0
+    
+//     for (let i = 1; i < num1; i++){
+//         if (num1 % i ==0){
+//             count +=i
+//         }
+//     }
+//     if (count == num1) {
+//         console.log(num1 , "murakkab son");
+//     }else{
+//         console.log(num1 , "oddiy son");
+//     } 
+    
+// }
+//     else{
+//         console.log("musbat son kiritilsin");
+//     }
+
+
+
+
+
+// ishoralarni topish
+
+// let suz =prompt("birnimalar yozing")
+// let a =true
+// let soniP = 0
+// let soniM = 0
+// let soniB = 0
+// let soniK = 0
+// for (let i = 0; i < suz.length; i++){
+    
+//     for(let j = 0; i>j; j++){
+//         if(suz[i] == "+" ){
+//             soniP+=1
+//             a = false
+//             console.log(suz[i] , "bu ishora",soniP,"-bulib kelganda",i+=1,"-bolib qatnashgan");
+        
+//         }else if(suz[i] == "-"){
+//             a = false
+//             console.log(suz[i], "bu ishora",soniM,"-bulib keladi",i+=1,"-bulib qantashadi");
+//         } else if(suz[i] == "+"){
+//             a = false
+//             console.log(suz[i],"bu ishora",soniK,"-bulib keladi",i+=1,"-bulib qantashadi");
+//         }else if(suz[i] == "*"){
+//             a = false
+//             console.log(suz[i], "bu ishora",soniB,"-bulib keladi",i+=1,"-bulib qantashadi");
+//         }else if(suz[i] == "/"){
+//             a = false
+//             console.log(suz[i], "bu ishora",soniP,"-bulib keladi",i+=1,"-bulib qantashadi");
+//         }
+        
+        
+//     }
+// }
+
+
+// if (a)("kiritilgan suzda ishora yuq");
